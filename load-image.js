3706cb74752d63162f9db445fed1a4ad7f91cc9b
sasaplus1 <sasaplus1@gmail.com>
2016-12-19 00:25:49 +0900

    updated scripts

--- load-image.js
+++ load-image.js
@@ -1,5 +1,5 @@
 /*!
- * @license load-image.js Copyright(c) 2016 sasa+1
+ * @license load-image.js ver.1.0.0 Copyright(c) 2016 sasa+1
  * https://github.com/sasaplus1-prototype/load-image.js
  * Released under the MIT license.
  */
@@ -61,8 +61,16 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	var once = __webpack_require__(1);
+	var isFunction = __webpack_require__(1);
 
+	var once = __webpack_require__(2);
+
+	/**
+	 * callback when image loaded
+	 *
+	 * @param {String} src
+	 * @param {Function} callback
+	 */
 	function load(src, callback) {
 	  var image = new Image();
 
@@ -78,54 +86,97 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	  image.src = src;
 
-	  if (image.naturalWidth) {
+	  if (image.naturalWidth || image.complete) {
 	    callback(null, image);
 	  }
 	}
 
-	module.exports = function(src, callback) {
-	  if (typeof callback === 'function') {
+	/**
+	 * load image
+	 *
+	 * @param {String} src
+	 * @param {Function} [callback]
+	 * @return {Promise}
+	 */
+	function loadImage(src, callback) {
+	  if (isFunction(callback)) {
 	    load(src, once(callback));
 	  } else {
-	    return new Promise(function(resolve, reject) {
+	    return new loadImage.Promise(function(resolve, reject) {
 	      load(src, function(err, image) {
 	        (err) ? reject(err) : resolve(image);
 	      });
 	    });
 	  }
-	};
+	}
+
+	// NOTE: get global object by any environment
+	loadImage.Promise = Function('return this')().Promise;
+
+	module.exports = loadImage;
 
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
-	/*!
-	 * @license once.js Copyright(c) 2016 sasa+1
-	 * https://github.com/sasaplus1-prototype/once.js
-	 * Released under the MIT license.
-	 */
 	'use strict';
 
-	module.exports = function(fn) {
+	var toString = Object.prototype.toString;
+
+	module.exports = function isFunction(value) {
+	  return (
+	    typeof value === 'function' || toString.call(value) === '[object Function]'
+	  );
+	};
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var isFunction = __webpack_require__(1);
+
+	/**
+	 * return converted function
+	 *
+	 * @param {Function} fn
+	 * @throws {TypeError}
+	 * @return {Function}
+	 */
+	module.exports = function once(fn) {
 	  var count;
 
-	  if (typeof fn !== 'function') {
+	  if (!isFunction(fn)) {
 	    throw new TypeError('fn must be a Function');
 	  }
 
 	  count = 1;
 
 	  return function() {
-	    var result;
+	    var args, call;
 
-	    if (count-- > 0) {
-	      result = fn.apply(this, arguments);
+	    if (count-- <= 0) {
+	      return;
 	    }
 
-	    fn = void 0;
+	    args = arguments;
+	    call = 'call';
 
-	    return result;
+	    switch (args.length) {
+	      case 0:
+	        return fn[call](this);
+	      case 1:
+	        return fn[call](this, args[0]);
+	      case 2:
+	        return fn[call](this, args[0], args[1]);
+	      case 3:
+	        return fn[call](this, args[0], args[1], args[2]);
+	      default:
+	        return fn.apply(this, args);
+	    }
 	  };
 	};
 
